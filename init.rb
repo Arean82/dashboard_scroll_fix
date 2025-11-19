2b13ee2c8845b470cf0cf3c510cdfd3fe372d100
arean82 <arean82@gmail.com>
2025-11-19 18:00:19 +0530

    removed hook updated init

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'redmine'
-require_relative 'lib/dashboard_scroll_fix/hooks'
+#require_relative 'lib/dashboard_scroll_fix/hooks'
 
 Redmine::Plugin.register :dashboard_scroll_fix do
   name 'Dashboard Scroll Fix'
@@ -7,15 +7,15 @@ Redmine::Plugin.register :dashboard_scroll_fix do
   url 'https://github.com/Arean82/dashboard_scroll_fix'
   author_url 'mailto:arean82@gmail.com'
 
+  description 'Adds scroll and auto-shrink for Dashboard plugin'
   version '1.0.0'
 
-  settings partial: 'settings/dashboard_scroll_fix_settings',
-           default:{
-             'auto_shrink'=>true,
-             'shrink_width_px'=>220,
-             'shrink_threshold'=>8,
-             'scroll_mode'=>'auto',
-             'scroll_size_mode'=>'auto',
-             'scroll_size_custom'=>'8'
-           }
+  settings default: {
+      autoShrink: true,
+      shrinkWidth: 220,
+      shrinkThreshold: 8,
+      scrollMode: 'auto',
+      scrollSizeMode: 'auto',
+      scrollSizeCustom: 8
+  }, partial: 'settings/dashboard_scroll_fix_settings'
 end
